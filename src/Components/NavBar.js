cb70658dc9bfa0c7489759be3dda335ddd25c728
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2023-11-22 23:13:21 -0500

    Made Changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,26 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';  
 
 const NavBar = () => {
  return (
- <nav>
-       <ul>
-          <li>
-             <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-             <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-             <NavLink to="/donate">Donate</NavLink>
-          </li>
-          <li>
-             <NavLink to="/gala">Gala</NavLink>
-          </li>
-       </ul>
- </nav>
+   <div> 
+      <nav class="bg-teal-400">
+          <ul>
+               <li >
+                  <NavLink to="/home">Home</NavLink>
+               </li>
+               <li>
+                  <NavLink to="/about">About</NavLink>
+               </li>
+               <li>
+                  <NavLink to="/donate">Donate</NavLink>
+               </li>
+               <li>
+                  <NavLink to="/gala">Gala</NavLink>
+               </li>
+            </ul>
+      </nav>
+   </div>
+ 
  );
 };
 
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Donate from './Donate';
 import About from './About';
 import Gala from './Gala';
+import Home from './Home';
 import NavBar from '../Components/NavBar';
 
 function App() {
@@ -9,10 +10,17 @@ function App() {
         <>
             <NavBar />
             <Routes>
+                <Route path="/home" element={<Home />} />
                 <Route path="/donate" element={<Donate />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/gala" element={<Gala />} />
             </Routes>
+            <div class="bg-blue-600">
+                <p class="text-red-500"> 
+                    abc
+                </p>
+            </div>
+            
         </>
     );
 }
