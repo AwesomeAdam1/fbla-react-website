cc9b3e454e7d5aa7fd96f6f913dd5fa3df0f1359
vipban <145408790+vipban@users.noreply.github.com>
2023-11-28 15:27:26 -0500

    Fix Navigation

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ function App() {
                     </div>
                     <div>
                         <ul class="menu-vertical lg:menu-vertical bg-base-300 rounded-box w-full">
-                            <li><NavLink to="/walton-fbla-website-design/home">Home</NavLink></li>
-                            <li><NavLink to="/walton-fbla-website-design/about">About</NavLink></li>
-                            <li><NavLink to="/walton-fbla-website-design/donate">Donate</NavLink></li>
-                            <li><NavLink to="/walton-fbla-website-design/upcomingevents">Upcoming Events</NavLink></li>
+                            <li><NavLink to="/home">Home</NavLink></li>
+                            <li><NavLink to="/about">About</NavLink></li>
+                            <li><NavLink to="/donate">Donate</NavLink></li>
+                            <li><NavLink to="/upcomingevents">Upcoming Events</NavLink></li>
                         </ul> 
                     </div>                    
                 </div>        
