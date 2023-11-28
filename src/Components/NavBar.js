78f93a73b3fe3dc7a778205a6e1a33a9bed0af28
vipban <145408790+vipban@users.noreply.github.com>
2023-11-28 15:23:59 -0500

    Finalize other stuff

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
  return (
    <div>
      <ul class="menu menu-vertical lg:menu-horizontal bg-base-300 rounded-box w-full">
-      <li><NavLink to="/walton-fbla-website-design/home">Home</NavLink></li>
-      <li><NavLink to="/walton-fbla-website-design/about">About</NavLink></li>
-      <li><NavLink to="/walton-fbla-website-design/donate">Donate</NavLink></li>
-      <li><NavLink to="/walton-fbla-website-design/upcomingevents">Upcoming Events</NavLink></li>
+      <li><NavLink to="/home">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/donate">Donate</NavLink></li>
+      <li><NavLink to="/upcomingevents">Upcoming Events</NavLink></li>
      </ul> 
    </div>
    
