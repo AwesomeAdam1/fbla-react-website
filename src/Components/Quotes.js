777ed953fa97af1a24ec8ce23da0b8f842057d5e
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2024-01-15 19:19:40 -0500

    scaling

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -1,21 +1,21 @@
 function Quotes(){
     return ( 
         <div className="text-center m-2"> 
-            <div className="card w-1/2 bg-base-100 shadow-xl m-2 inline-block">
+            <div className="card w-1/3 bg-base-100 shadow-xl m-2 inline-block">
                 <div className="card-body">
-                    <h2 className="card-title">Jesse the nerd</h2>
+                    <h2 className="card-title">Jesse</h2>
                     <p>"Walton Habitat for Humanity has provided help and support for the community."</p>
                 </div>
             </div>
 
-            <div className="card w-96 bg-base-100 shadow-xl m-2 inline-block">
+            <div className="card w-1/3 bg-base-100 shadow-xl m-2 inline-block">
                 <div className="card-body">
                     <h2 className="card-title">Arnav</h2>
                     <p>"I enjoy helping out and voluteering for Walton Habitat."</p>
                 </div>
             </div>
 
-            <div className="card w-96 bg-base-100 shadow-xl m-2 inline-block">
+            <div className="card w-1/3 bg-base-100 shadow-xl m-2 inline-block">
                 <div className="card-body">
                     <h2 className="card-title">Harry</h2>
                     <p>"I love the work Habitat does for the community."</p>
