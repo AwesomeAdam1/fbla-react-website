8c88f22a13cdcf0d570a29098f0c431cedbe7a17
vipban <145408790+vipban@users.noreply.github.com>
2024-01-16 13:33:28 -0500

    Fix coloring

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
                         Walton Habitat For Humanity
                     </div>
                     <div>
-                        <ul class="menu-vertical lg:menu-vertical bg-base-300 rounded-box w-full">
+                        <ul class="menu-vertical lg:menu-vertical w-full">
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/about">About</NavLink></li>
                             <li><NavLink to="/donate">Donate</NavLink></li>
