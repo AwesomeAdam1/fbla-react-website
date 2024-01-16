43a127961944d24d73c488b4fbcdc696cef12065
vipban <145408790+vipban@users.noreply.github.com>
2024-01-16 13:24:55 -0500

    Fix NavBar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,91 +1,44 @@
-// import Quotes from '../Components/Quotes';
-// import RecentEvents from '../Components/RecentEvents';
-// import UpcomingEvents from '../Components/UpcomingEvents';
-// import habitatLogo from '../Resources/HabitatLogo.jpeg';
+import Quotes from '../Components/Quotes';
+import RecentEvents from '../Components/RecentEvents';
+import UpcomingEvents from '../Components/UpcomingEvents';
+import habitatLogo from '../Resources/HabitatLogo.jpeg';
 
-// import './Stylesheets/Home.css'
+import './Stylesheets/Home.css'
 
-// function Home() {
-//   return (
-//     <>
-//     <img src={habitatLogo} className='logo'></img>
-//     <div class="flex flex-col w-full">
-//       <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
-//         <div class="text-center">
-//           <p class="text-7xl m-12">
-//             Upcoming Events
-//           </p>
-//         </div>
-//         <UpcomingEvents />
-//       </div> 
-//       <div class="divider w-2/3 mx-auto"></div> 
-//       <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
-//         <div class="text-center">
-//           <p class="text-7xl m-12">
-//             Recent Events
-//           </p>
-//         </div>
-//         <RecentEvents />
-//       </div>
-//       <div class="divider w-2/3 mx-auto"></div> 
-//       <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
-//       <div class="text-center">
-//           <p class="text-5xl m-12">
-//             Volunteer Quotes
-//           </p>
-//         </div>
-//         <Quotes />
-//       </div>
-//     </div>
-//     </>   
-//   );
-// }
-
-// export default Home;
-
-import { Link, useMatch, useResolvedPath } from 'react-router-dom'
-import './Stylesheets/header.css'
-
-export default function Header() {
-    function handleClick() {
-        document.getElementById('hamburger-toggle').click()
-    }
-
-    return (
-        <nav className='nav'>
-            <Link to='/' className='site-title'>Walton CSHS</Link>
-            <div className='mobile-nav'>
-                <label className='hamburger-menu'>
-                    <input type='checkbox' name='' id='hamburger-toggle' />
-                </label>
-                <aside className='sidebar'>
-                    <ul>
-                        <CustomLink to='/' onClick={handleClick}>Home</CustomLink>
-                        <CustomLink to='about' onClick={handleClick}>About</CustomLink>
-                        <CustomLink to='donate' onClick={handleClick}>Donate</CustomLink>
-                        <CustomLink to='upcomingevents' onClick={handleClick}>Upcoming Events</CustomLink>
-                    </ul>
-                </aside>
-            </div>
-            <ul className='desktop-nav'>
-                <CustomLink to='/'>Home</CustomLink>
-                <CustomLink to='about'>About</CustomLink>
-                <CustomLink to='donate'>Donate</CustomLink>
-                <CustomLink to='upcomingevents'>Upcoming Events</CustomLink>
-            </ul>
-        </nav>
-    )
+function Home() {
+  return (
+    <>
+    <img src={habitatLogo} className='logo'></img>
+    <div class="flex flex-col w-full">
+      <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
+        <div class="text-center">
+          <p class="text-7xl m-12">
+            Upcoming Events
+          </p>
+        </div>
+        <UpcomingEvents />
+      </div> 
+      <div class="divider w-2/3 mx-auto"></div> 
+      <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
+        <div class="text-center">
+          <p class="text-7xl m-12">
+            Recent Events
+          </p>
+        </div>
+        <RecentEvents />
+      </div>
+      <div class="divider w-2/3 mx-auto"></div> 
+      <div class="grid h-full card bg-base-300 rounded-box place-items-center m-10">
+      <div class="text-center">
+          <p class="text-5xl m-12">
+            Volunteer Quotes
+          </p>
+        </div>
+        <Quotes />
+      </div>
+    </div>
+    </>   
+  );
 }
 
-function CustomLink({ to, children, ...props}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
-    return (
-        <li className={isActive ? 'active' : ''}>
-            <Link to={to} {...props}>
-                {children}
-            </Link>
-        </li>
-    )
-}
+export default Home;
